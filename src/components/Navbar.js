a657f88d4500940371023c37a173e11a4409cdd0
evabujazha <evabujazha@gmail.com>
2022-08-30 23:17:29 -0300

    navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import "../App.css";
+
+const Navbar = () => {
+  return (
+    <div>
+      <header>
+        <h1>Licencias Arg</h1>
+        <nav>
+          <a href="/#">Home</a>
+          <a href="/#">Sistema Operativo</a>
+          <a href="/#">Software</a>
+          <a href="/#">Juegos</a>
+        </nav>
+      </header>
+    </div>
+  );
+};
+
+export default Navbar;
